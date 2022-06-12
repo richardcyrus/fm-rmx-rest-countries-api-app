e59fc196252d6e3ff5727d03a417c52d058ca7a4
Richard Cyrus <richard.cyrus@rcyrus.com>
2022-06-12 13:32:03 -0400

    Enhance API mock to return requested fields

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,24 +1,41 @@
 const { rest } = require('msw');
 
-const countries = require('./data/all-v2.json');
+const countries = require('./data/countries.json');
 
 const endpoint = 'https://restcountries.com/v2';
 
 const handlers = [
   /* listAllCountries(), getRegionList() */
   rest.get(`${endpoint}/all`, (req, res, ctx) => {
-    // const fields = req.url.searchParams.get('fields');
-    return res(ctx.status(200), ctx.json(countries));
+    const fields = req.url.searchParams.get('fields').split(',');
+
+    let results = countries.reduce((accumulator, item) => {
+      let country = {};
+
+      fields.forEach((field) => (country[field] = item[field]));
+      accumulator.push(country);
+
+      return accumulator;
+    }, []);
+
+    return res(ctx.status(200), ctx.json(results));
   }),
 
-  /* searchCountryByName(name) */
-  rest.get(`${endpoint}/name/:name`, (req, res, ctx) => {
-    const { name: pname } = req.params;
-    // const fields = req.url.searchParams.get('fields');
+  /* searchCountryByName(partialName) */
+  rest.get(`${endpoint}/name/:partialName`, (req, res, ctx) => {
+    const { partialName } = req.params;
+    const fields = req.url.searchParams.get('fields').split(',');
 
-    let results = countries.filter((item) =>
-      item.name.toLowerCase().includes(pname)
-    );
+    let results = countries.reduce((accumulator, item) => {
+      let country = {};
+
+      if (item.name.toLowerCase().includes(partialName)) {
+        fields.forEach((field) => (country[field] = item[field]));
+        accumulator.push(country);
+      }
+
+      return accumulator;
+    }, []);
 
     return res(ctx.status(200), ctx.json(results));
   }),
@@ -26,11 +43,18 @@ const handlers = [
   /* listCountriesInRegion(region) */
   rest.get(`${endpoint}/region/:region`, (req, res, ctx) => {
     const { region } = req.params;
-    // const fields = req.url.searchParams.get('fields');
+    const fields = req.url.searchParams.get('fields').split(',');
 
-    let results = countries.filter((item) =>
-      item.region.toLowerCase().includes(region)
-    );
+    let results = countries.reduce((accumulator, item) => {
+      let country = {};
+
+      if (item.region.toLowerCase().includes(region)) {
+        fields.forEach((field) => (country[field] = item[field]));
+        accumulator.push(country);
+      }
+
+      return accumulator;
+    }, []);
 
     return res(ctx.status(200), ctx.json(results));
   }),
@@ -38,9 +62,14 @@ const handlers = [
   /* getBorderCountryInfo(code), getCountryDetailByCode(code) */
   rest.get(`${endpoint}/alpha/:code`, (req, res, ctx) => {
     const { code } = req.params;
-    // const fields = req.url.searchParams.get('fields');
+    const fields = req.url.searchParams.get('fields').split(',');
 
-    let result = countries.find((item) => item.alpha3Code === code);
+    let result = countries.reduce((accumulator, item) => {
+      if (item.alpha2Code === code || item.alpha3Code === code) {
+        fields.forEach((f) => (accumulator[f] = item[f]));
+      }
+      return accumulator;
+    }, {});
 
     return res(ctx.status(200), ctx.json(result));
   }),
--- test/setup-test-env.ts
+++ test/setup-test-env.ts
@@ -1,4 +1,7 @@
 import { installGlobals } from '@remix-run/node/globals';
 import '@testing-library/jest-dom/extend-expect';
 
+import server from '@/mocks/server';
+
 installGlobals();
+server.listen({ onUnhandledRequest: 'bypass' });
